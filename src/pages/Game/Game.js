4609d5a71c19f13e038727a448d5b34d7282c1b5
SPatel144 <fweep321@gmail.com>
2023-01-24 16:56:29 -0500

    Added code for saving preferences

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,86 +1,85 @@
-import React, { useEffect, useState, useHistory } from 'react'
-import fetchFromSpotify, { request } from '../../services/api'
-import './Game.css'
+import React, { useEffect, useState, useHistory } from "react";
+import fetchFromSpotify, { request } from "../../services/api";
+import "./Game.css";
 
-import Player from '../../components/Player/Player'
+import Player from "../../components/Player/Player";
 
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 const AUTH_ENDPOINT =
-  'https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token'
-const TOKEN_KEY = 'whos-who-access-token'
+  "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
+const TOKEN_KEY = "whos-who-access-token";
 
 const Game = () => {
-  const [artists, setArtists] = useState([])
-  const [songs, setSongs] = useState([])
-  const [token, setToken] = useState('')
-  const [authLoading, setAuthLoading] = useState(false)
-  const [configLoading, setConfigLoading] = useState(false)
-  const [genre] = useState(JSON.parse(localStorage.getItem('genre')))
-  const [artistNum] = useState(JSON.parse(localStorage.getItem('artist')))
-  const [songNum] = useState(JSON.parse(localStorage.getItem('song')))
-  const num = songNum.song
-  const imgNum = artistNum.artist
+  const [artists, setArtists] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [token, setToken] = useState("");
+  const [authLoading, setAuthLoading] = useState(false);
+  const [configLoading, setConfigLoading] = useState(false);
+  const [genre] = useState(JSON.parse(localStorage.getItem("genre")));
+  const [artistNum] = useState(JSON.parse(localStorage.getItem("artist")));
+  const [songNum] = useState(JSON.parse(localStorage.getItem("song")));
+  const num = songNum.song;
+  const imgNum = artistNum.artist;
 
   useEffect(() => {
-    setAuthLoading(true)
-    const storedTokenString = localStorage.getItem(TOKEN_KEY)
+    setAuthLoading(true);
+    const storedTokenString = localStorage.getItem(TOKEN_KEY);
     if (storedTokenString) {
-      const storedToken = JSON.parse(storedTokenString)
+      const storedToken = JSON.parse(storedTokenString);
       if (storedToken.expiration > Date.now()) {
-        console.log('Token found in localstorage')
-        setAuthLoading(false)
-        setToken(storedToken.value)
-        loadData(storedToken.value)
-        return
+        console.log("Token found in localstorage");
+        setAuthLoading(false);
+        setToken(storedToken.value);
+        loadData(storedToken.value);
+        return;
       }
     }
-    console.log('Sending request to AWS endpoint')
+    console.log("Sending request to AWS endpoint");
     request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
       const newToken = {
         value: access_token,
         expiration: Date.now() + (expires_in - 20) * 1000,
-      }
-      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken))
-      setAuthLoading(false)
-      setToken(newToken.value)
-      loadData(newToken.value)
-    })
-  }, [])
+      };
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
+      setAuthLoading(false);
+      setToken(newToken.value);
+      loadData(newToken.value);
+    });
+  }, []);
 
   const randomIndex = function () {
-    return Math.floor(Math.random() * imgNum)
-  }
+    return Math.floor(Math.random() * imgNum);
+  };
 
-  const random = randomIndex()
+  const random = randomIndex();
 
-  let randomOffset = Math.floor(Math.random() * 100)
+  let randomOffset = Math.floor(Math.random() * 100);
 
   const loadData = async (t) => {
-    setConfigLoading(true)
+    setConfigLoading(true);
     const response = await fetchFromSpotify({
       token: t,
       endpoint: `search?q=genre:${genre.genre}&type=artist&offset=${randomOffset}`,
-    })
+    });
 
-    let data = await response.artists.items[random].id
+    let data = await response.artists.items[random].id;
 
-    setArtists(response.artists.items)
+    setArtists(response.artists.items);
 
     const response2 = await fetchFromSpotify({
       token: t,
       endpoint: `artists/${data}/top-tracks?market=US`,
-    })
+    });
 
-    let data2 = await response2.tracks
+    let data2 = await response2.tracks;
 
-    setSongs(data2.filter((song) => song.preview_url != null))
-    setConfigLoading(false)
-  }
+    setSongs(data2.filter((song) => song.preview_url != null));
+    setConfigLoading(false);
+  };
 
   if (authLoading || configLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   // Check if artist name is the same as artist name in song data
@@ -88,69 +87,77 @@ const Game = () => {
   // function checkGuess() {
   //   if (artists[random].name === songs[random])
   // }
- 
-  console.log(artists[random])
-  console.log(songs[random])
+
+  console.log(artists[random]);
+  console.log(songs[random]);
 
   return (
-      <div className='container mainContainer'>
-        <div className='row'>
-          <div className='col'>
-            <div className="jumbotron jumbotron-fluid">
-              <div className="container">
-                <h1 className="display-4"> Guess !!</h1>
-                <div className='row'>
-                  <div className='col xs-6 sm-4'>
-                    <p className="lead">Play the song and guess the artist. </p>
-                  </div>
-                  <div className='col xs-6 sm-8'>
-                    <button className="btn btn-info btn-style2 mt-2">
-                      <Link to="/" style={{ textDecoration: 'none', color: 'white' }}> Home </Link>
-                    </button>
-                  </div>
+    <div className="container mainContainer">
+      <div className="row">
+        <div className="col">
+          <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+              <h1 className="display-4"> Guess !!</h1>
+              <div className="row">
+                <div className="col xs-6 sm-4">
+                  <p className="lead">Play the song and guess the artist. </p>
                 </div>
-                <hr className="my-4" />
-                <div className='row'>
-                  {artists.length > imgNum
-                    ? artists.slice(0, imgNum).map((artist) => (
+                <div className="col xs-6 sm-8">
+                  <button className="btn btn-info btn-style2 mt-2">
+                    <Link
+                      to="/"
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      {" "}
+                      Home{" "}
+                    </Link>
+                  </button>
+                </div>
+              </div>
 
-                      <div className='col' key={artist.id}>
-                        <img src={artist.images[0].url} className="rounded-lg sizing" />
+              <hr className="my-4" />
+
+              <div className="row">
+                {artists.length > imgNum
+                  ? artists.slice(0, imgNum).map((artist) => (
+                      <div className="col" key={artist.id}>
+                        <img
+                          src={artist.images[0].url}
+                          className="rounded-lg sizing"
+                        />
                         <p>{artist.name}</p>
                       </div>
                     ))
-                    : 'No Images to display'}
-                </div>
-                <div className='row mt-4'>
-                  <ul className="list-unstyled">
-                    <li className="media">
-                      <div className="media-body">
-                        <h5 className="col mt-0 mb-3 ml-4"> Play Tracks </h5>
-                        <div className='col'>
-                          <div className='buttonContainer'>
+                  : "No Images to display"}
+              </div>
+              <div className="row mt-4">
+                <ul className="list-unstyled">
+                  <li className="media">
+                    <div className="media-body">
+                      <h5 className="col mt-0 mb-3 ml-4"> Play Tracks </h5>
+
+                      <div className="col">
+                        <div className="buttonContainer">
                           {songs.length > 0 ? (
                             songs
                               .slice(0, num)
-                              .map((song) => (
-                                <Player song={song} />
-                              ))
-                            ) : (
+                              .map((song) => <Player song={song} />)
+                          ) : (
                             <button onClick={() => window.location.reload()}>
                               No Songs for this Artist: Try again?
                             </button>
                           )}
-                          </div>
                         </div>
                       </div>
-                    </li>
-                  </ul>
-                </div>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
-  )
-}
-
-export default Game
+    </div>
+  );
+};
+export default Game;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,71 +1,121 @@
-import React, { useState } from 'react'
-import './Home.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Home = () => {
+  const [genre, setGenre] = useState(null);
+  const [artist, setArtist] = useState(null);
+  const [song, setSong] = useState(null);
 
-  const [genre, setGenre] = useState({ genre: "pop" });
-  const [artist, setArtist] = useState({ artist: 2 });
-  const [song, setSong] = useState({ song: 1 });
-
-  const handleSubmit = (e) => {
-    localStorage.setItem("genre", JSON.stringify(genre));
-    localStorage.setItem("artist", JSON.stringify(artist));
-    localStorage.setItem("song", JSON.stringify(song));
-  };
+  // const handleSubmit = (e) => {
+  //   localStorage.setItem("genre", JSON.stringify(genre));
+  //   localStorage.setItem("artist", JSON.stringify(artist));
+  //   localStorage.setItem("song", JSON.stringify(song));
+  // };
 
   const handleGenreSelect = (e) => {
-    setGenre({ genre: e.target.value })
-    console.log("genre: " + e.target.value)
+    setGenre(e.target.value);
+    console.log("genre: " + e.target.value);
   };
 
   const handleArtistSelect = (e) => {
-    setArtist({ artist: parseInt(e.target.value) })
-    console.log("artist: " + e.target.value)
+    setArtist(e.target.value);
+    console.log("artist: " + e.target.value);
   };
 
   const handleSongSelect = (e) => {
-    setSong({ song: parseInt(e.target.value) })
-    console.log("song: " + e.target.value)
+    setSong(e.target.value);
+    console.log("song: " + e.target.value);
   };
 
-  return (
+  useEffect(() => {
+    if (song === null) {
+      const savedSongs = JSON.parse(localStorage.getItem("songsKey"));
+      setSong(savedSongs != null ? savedSongs : 1);
+    }
+    localStorage.setItem("songsKey", JSON.stringify(song));
+  }, [song]);
 
-    <div className='container-fluid mainContainer'>
-      <div className='row'>
-        <div className='col'>
-          <h1 className='header display-4'> Who's who </h1>
+  useEffect(() => {
+    if (artist === null) {
+      const savedArtists = JSON.parse(localStorage.getItem("artistsKey"));
+      setArtist(savedArtists != null ? savedArtists : 2);
+    }
+    localStorage.setItem("artistsKey", JSON.stringify(artist));
+  }, [artist]);
+
+  useEffect(() => {
+    if (genre === null) {
+      const savedGenre = JSON.parse(localStorage.getItem("genreKey"));
+      setGenre(savedGenre != null ? savedGenre : "");
+    }
+    localStorage.setItem("genreKey", JSON.stringify(genre));
+  }, [genre]);
+
+  return (
+    <div className="container-fluid mainContainer">
+      <div className="row">
+        <div className="col">
+          <h1 className="header display-4"> Who's who </h1>
         </div>
       </div>
       <br />
       <br />
 
-      <div className='jumbotron mt-4'>
-
-        <div className='container'>
-
-          <div className='row'>
-            <div className='col'>
-              <form action="/game" className="form-inline" onSubmit={handleSubmit}>
-                <label className="my-1 mr-2" for="inlineFormCustomSelectPref"> Genre </label>
-                <select className="custom-select my-1 mr-sm-2" id="mySelect" defaultValue="pop" onChange={handleGenreSelect} required>
+      <div className="jumbotron mt-4">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <form
+                action="/game"
+                className="form-inline"
+                // onSubmit={handleSubmit}
+              >
+                <label className="my-1 mr-2" for="inlineFormCustomSelectPref">
+                  {" "}
+                  Genre{" "}
+                </label>
+                <select
+                  className="custom-select my-1 mr-sm-2"
+                  id="mySelect"
+                  value={genre}
+                  onChange={handleGenreSelect}
+                  required
+                >
                   <option selected>Choose...</option>
                   <option value="pop">pop</option>
                   <option value="hip-hop">hip-hop</option>
                   <option value="rock">rock</option>
                 </select>
 
-                <label className="my-1 mr-2" for="inlineFormCustomSelectPref"> Number of Artists </label>
-                <select className="custom-select my-1 mr-sm-2" id="mySelect" defaultValue="2" onChange={handleArtistSelect} required>
+                <label className="my-1 mr-2" for="inlineFormCustomSelectPref">
+                  {" "}
+                  Number of Artists{" "}
+                </label>
+                <select
+                  className="custom-select my-1 mr-sm-2"
+                  id="mySelect"
+                  value={artist}
+                  onChange={handleArtistSelect}
+                  required
+                >
                   <option selected>Choose...</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
                   <option value="4">4</option>
                 </select>
 
-                <label className="my-1 mr-2" for="inlineFormCustomSelectPref"> Number of Songs </label>
-                <select className="custom-select my-1 mr-sm-2" id="mySelect" defaultValue="1" onChange={handleSongSelect} required>
+                <label className="my-1 mr-2" for="inlineFormCustomSelectPref">
+                  {" "}
+                  Number of Songs{" "}
+                </label>
+                <select
+                  className="custom-select my-1 mr-sm-2"
+                  id="mySelect"
+                  value={song}
+                  onChange={handleSongSelect}
+                  required
+                >
                   <option selected>Choose...</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -73,21 +123,34 @@ const Home = () => {
                 </select>
 
                 <div class="custom-control custom-checkbox my-1 mr-sm-2 mt-3">
-                  <input type="checkbox" className="custom-control-input" id="customControlInline" />
-                  <label className="custom-control-label" for="customControlInline">Remember my preferences</label>
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="customControlInline"
+                  />
+                  <label
+                    className="custom-control-label"
+                    for="customControlInline"
+                  >
+                    Remember my preferences
+                  </label>
                 </div>
 
-                <button type="submit" id='submit' class="btn btn-info btn-style mt-2" required>Play</button>
+                <button
+                  type="submit"
+                  id="submit"
+                  class="btn btn-info btn-style mt-2"
+                  required
+                >
+                  Play
+                </button>
               </form>
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
