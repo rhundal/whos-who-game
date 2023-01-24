5a7868e4b3b15532865b4dfafd87b55659125447
Martin Hu <martin.m.hu89@gmail.com>
2023-01-24 16:04:53 -0500

    genres combobox working, pulling from spotify api. data transfers over pages to game.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import fetchFromSpotify, { request } from '../../services/api'
+
+const AUTH_ENDPOINT =
+  'https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token'
+const TOKEN_KEY = 'whos-who-access-token'
 import './Home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +13,52 @@ const Home = () => {
   const [genre, setGenre] = useState({ genre: "pop" });
   const [artist, setArtist] = useState({ artist: 2 });
   const [song, setSong] = useState({ song: 1 });
+  const [genres, setGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState('')
+  const [authLoading, setAuthLoading] = useState(false)
+  const [configLoading, setConfigLoading] = useState(false)
+  const [token, setToken] = useState('')
+
+  const loadGenres = async t => {
+    setConfigLoading(true)
+    const response = await fetchFromSpotify({
+      token: t,
+      endpoint: 'recommendations/available-genre-seeds'
+    })
+    console.log(response)
+    setGenres(response.genres)
+    setConfigLoading(false)
+  }
+
+  useEffect(() => {
+    setAuthLoading(true)
+    const storedTokenString = localStorage.getItem(TOKEN_KEY)
+    if (storedTokenString) {
+      const storedToken = JSON.parse(storedTokenString)
+      if (storedToken.expiration > Date.now()) {
+        console.log('Token found in localstorage')
+        setAuthLoading(false)
+        setToken(storedToken.value)
+        loadGenres(storedToken.value)
+        return
+      }
+    }
+    console.log('Sending request to AWS endpoint')
+    request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
+      const newToken = {
+        value: access_token,
+        expiration: Date.now() + (expires_in - 20) * 1000
+      }
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken))
+      setAuthLoading(false)
+      setToken(newToken.value)
+      loadGenres(newToken.value)
+    })
+  }, [])
+
+  if (authLoading || configLoading) {
+    return <div>Loading...</div>
+  }
 
   const handleSubmit = (e) => {
     localStorage.setItem("genre", JSON.stringify(genre));
@@ -17,6 +68,7 @@ const Home = () => {
 
   const handleGenreSelect = (e) => {
     setGenre({ genre: e.target.value })
+    setSelectedGenre(e.target.value)
     console.log("genre: " + e.target.value)
   };
 
@@ -40,22 +92,20 @@ const Home = () => {
       </div>
       <br />
       <br />
-
       <div className='jumbotron mt-4'>
-
         <div className='container'>
-
           <div className='row'>
             <div className='col'>
               <form action="/game" className="form-inline" onSubmit={handleSubmit}>
                 <label className="my-1 mr-2" for="inlineFormCustomSelectPref"> Genre </label>
-                <select className="custom-select my-1 mr-sm-2" id="mySelect" defaultValue="pop" onChange={handleGenreSelect} required>
-                  <option selected>Choose...</option>
-                  <option value="pop">pop</option>
-                  <option value="hip-hop">hip-hop</option>
-                  <option value="rock">rock</option>
+                <select value={selectedGenre} onChange={handleGenreSelect}>
+                  <option value='' />
+                  {genres.map(genre => (
+                    <option key={genre} value={genre}>
+                      {genre}
+                    </option>
+                  ))}
                 </select>
-
                 <label className="my-1 mr-2" for="inlineFormCustomSelectPref"> Number of Artists </label>
                 <select className="custom-select my-1 mr-sm-2" id="mySelect" defaultValue="2" onChange={handleArtistSelect} required>
                   <option selected>Choose...</option>
@@ -63,7 +113,6 @@ const Home = () => {
                   <option value="3">3</option>
                   <option value="4">4</option>
                 </select>
-
                 <label className="my-1 mr-2" for="inlineFormCustomSelectPref"> Number of Songs </label>
                 <select className="custom-select my-1 mr-sm-2" id="mySelect" defaultValue="1" onChange={handleSongSelect} required>
                   <option selected>Choose...</option>
@@ -71,12 +120,10 @@ const Home = () => {
                   <option value="2">2</option>
                   <option value="3">3</option>
                 </select>
-
                 <div class="custom-control custom-checkbox my-1 mr-sm-2 mt-3">
                   <input type="checkbox" className="custom-control-input" id="customControlInline" />
                   <label className="custom-control-label" for="customControlInline">Remember my preferences</label>
                 </div>
-
                 <button type="submit" id='submit' class="btn btn-info btn-style mt-2" required>Play</button>
               </form>
             </div>
