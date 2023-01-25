26e6de9977c23d65cc0bc73d57dd91333aaae725
Sejo Basic <sejoob@gmail.com>
2023-01-24 17:45:04 -0700

    Updated Styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
+
 import React, { useEffect, useState } from "react";
 import fetchFromSpotify, { request } from "../../services/api";
-import loader from "../../assets/loading.svg";
+import loader from '../../assets/loading.svg'
 
 const AUTH_ENDPOINT =
   "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
@@ -70,81 +71,92 @@ const Home = () => {
       const newToken = {
         value: access_token,
         expiration: Date.now() + (expires_in - 20) * 1000,
-      };
-      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
-      setAuthLoading(false);
-      setToken(newToken.value);
-      loadGenres(newToken.value);
-    });
-  }, []);
+      }
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken))
+      setAuthLoading(false)
+      setToken(newToken.value)
+      loadGenres(newToken.value)
+    })
+  }, [])
 
   if (authLoading || configLoading) {
     return (
-      <div className="loader">
+      <div className='loader'>
         <img src={loader} />
       </div>
-    );
+    )
+  }
+
+  const handleSubmit = (e) => {
+    localStorage.setItem('genre', JSON.stringify(genre))
+    localStorage.setItem('artist', JSON.stringify(artist))
+    localStorage.setItem('song', JSON.stringify(song))
   }
 
   const handleGenreSelect = (e) => {
-    setGenre({ genre: e.target.value });
-    setSelectedGenre(e.target.value);
-    console.log("genre: " + e.target.value);
-  };
+    setGenre({ genre: e.target.value })
+    setSelectedGenre(e.target.value)
+    console.log('genre: ' + e.target.value)
+  }
 
   const handleArtistSelect = (e) => {
-    setArtist(e.target.value);
-    console.log("artist: " + e.target.value);
-  };
+    setArtist({ artist: parseInt(e.target.value) })
+    console.log('artist: ' + e.target.value)
+  }
 
   const handleSongSelect = (e) => {
-    setSong(e.target.value);
-    console.log("song: " + e.target.value);
-  };
+    setSong({ song: parseInt(e.target.value) })
+    console.log('song: ' + e.target.value)
+  }
 
   return (
-    <div className="home-container">
-      <form action="/game">
-        <label for="inlineFormCustomSelectPref"> Genre </label>
+    <div className='home-container'>
+      <form action='/game' onSubmit={handleSubmit}>
+        <label for='inlineFormCustomSelectPref'> Genre </label>
         <select value={selectedGenre} onChange={handleGenreSelect}>
-          <option value="" />
+          <option value='' />
           {genres.map((genre) => (
             <option key={genre} value={genre}>
               {genre}
             </option>
           ))}
         </select>
-        <label for="inlineFormCustomSelectPref"> Number of Artists </label>
+        <label for='inlineFormCustomSelectPref'> Number of Artists </label>
         <select
-          value={artist}
+          defaultValue='2'
           onChange={handleArtistSelect}
-          type="number"
+          type='number'
           required
         >
           <option selected disabled>
             Choose
           </option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
         </select>
-        <label for="inlineFormCustomSelectPref"> Number of Songs </label>
-        <select value={song} onChange={handleSongSelect} type="number" required>
+        <label for='inlineFormCustomSelectPref'> Number of Songs </label>
+        <select
+          defaultValue='1'
+          onChange={handleSongSelect}
+          type='number'
+          required
+        >
           <option selected disabled>
             Choose
           </option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
         </select>
         <div>
-          <button className="play-btn" type="submit" id="submit" required>
+          <button className='play-btn' type='submit' id='submit' required>
             Play
           </button>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
