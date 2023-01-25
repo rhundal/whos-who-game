9ff1ccf0cb71360d57ba9ce67bd02a81dd70ba72
Sejo Basic <sejoob@gmail.com>
2023-01-25 13:12:26 -0700

    Fixed song and artist state back to default

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,151 +1,149 @@
-import React, { useEffect, useState } from "react";
-import fetchFromSpotify, { request } from "../../services/api";
-import loader from "../../assets/loading.svg";
+import React, { useEffect, useState } from 'react'
+import fetchFromSpotify, { request } from '../../services/api'
+import loader from '../../assets/loading.svg'
 
 const AUTH_ENDPOINT =
-  "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
-const TOKEN_KEY = "whos-who-access-token";
-import "./Home.css";
+  'https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token'
+const TOKEN_KEY = 'whos-who-access-token'
+import './Home.css'
 
 const Home = () => {
-  const [genre, setGenre] = useState({ genre: "pop" });
-  const [artist, setArtist] = useState(
-    JSON.parse(localStorage.getItem("artist"))
-  );
-  const [song, setSong] = useState(JSON.parse(localStorage.getItem("song")));
-  const [genres, setGenres] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState("");
-  const [authLoading, setAuthLoading] = useState(false);
-  const [configLoading, setConfigLoading] = useState(false);
-  const [token, setToken] = useState("");
+  const [genre, setGenre] = useState({ genre: 'pop' })
+  const [artist, setArtist] = useState({ artist: 2 })
+  const [song, setSong] = useState({ song: 1 })
+  const [genres, setGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState('')
+  const [authLoading, setAuthLoading] = useState(false)
+  const [configLoading, setConfigLoading] = useState(false)
+  const [token, setToken] = useState('')
 
   useEffect(() => {
-    localStorage.setItem("genre", JSON.stringify(genre));
-    localStorage.setItem("artist", JSON.stringify(artist));
-    localStorage.setItem("song", JSON.stringify(song));
-  });
+    localStorage.setItem('genre', JSON.stringify(genre))
+    localStorage.setItem('artist', JSON.stringify(artist))
+    localStorage.setItem('song', JSON.stringify(song))
+  })
 
   const loadGenres = async (t) => {
-    setConfigLoading(true);
+    setConfigLoading(true)
     const response = await fetchFromSpotify({
       token: t,
-      endpoint: "recommendations/available-genre-seeds",
-    });
-    console.log(response);
-    setGenres(response.genres);
-    setConfigLoading(false);
-  };
+      endpoint: 'recommendations/available-genre-seeds',
+    })
+    console.log(response)
+    setGenres(response.genres)
+    setConfigLoading(false)
+  }
 
   useEffect(() => {
-    setAuthLoading(true);
-    const storedTokenString = localStorage.getItem(TOKEN_KEY);
+    setAuthLoading(true)
+    const storedTokenString = localStorage.getItem(TOKEN_KEY)
     if (storedTokenString) {
-      const storedToken = JSON.parse(storedTokenString);
+      const storedToken = JSON.parse(storedTokenString)
       if (storedToken.expiration > Date.now()) {
-        console.log("Token found in localstorage");
-        setAuthLoading(false);
-        setToken(storedToken.value);
-        loadGenres(storedToken.value);
-        return;
+        console.log('Token found in localstorage')
+        setAuthLoading(false)
+        setToken(storedToken.value)
+        loadGenres(storedToken.value)
+        return
       }
     }
-    console.log("Sending request to AWS endpoint");
+    console.log('Sending request to AWS endpoint')
     request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
       const newToken = {
         value: access_token,
         expiration: Date.now() + (expires_in - 20) * 1000,
-      };
-      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
-      setAuthLoading(false);
-      setToken(newToken.value);
-      loadGenres(newToken.value);
-    });
-  }, []);
+      }
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken))
+      setAuthLoading(false)
+      setToken(newToken.value)
+      loadGenres(newToken.value)
+    })
+  }, [])
 
   if (authLoading || configLoading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   const handleSubmit = (e) => {
-    localStorage.setItem("genre", JSON.stringify(genre));
-    localStorage.setItem("artist", JSON.stringify(artist));
-    localStorage.setItem("song", JSON.stringify(song));
-  };
+    localStorage.setItem('genre', JSON.stringify(genre))
+    localStorage.setItem('artist', JSON.stringify(artist))
+    localStorage.setItem('song', JSON.stringify(song))
+  }
 
   const handleGenreSelect = (e) => {
-    setGenre({ genre: e.target.value });
-    setSelectedGenre(e.target.value);
-    console.log("genre: " + e.target.value);
-  };
+    setGenre({ genre: e.target.value })
+    setSelectedGenre(e.target.value)
+    console.log('genre: ' + e.target.value)
+  }
 
   const handleArtistSelect = (e) => {
-    setArtist({ artist: parseInt(e.target.value) });
-    console.log("artist: " + e.target.value);
-  };
+    setArtist({ artist: parseInt(e.target.value) })
+    console.log('artist: ' + e.target.value)
+  }
 
   const handleSongSelect = (e) => {
-    setSong({ song: parseInt(e.target.value) });
-    console.log("song: " + e.target.value);
-  };
+    setSong({ song: parseInt(e.target.value) })
+    console.log('song: ' + e.target.value)
+  }
 
   if (authLoading || configLoading) {
     return (
-      <div className="loader">
+      <div className='loader'>
         <img src={loader} />
       </div>
-    );
+    )
   }
 
   return (
-    <div className="home-container">
-      <form action="/game" onSubmit={handleSubmit}>
-        <label htmlFor="inlineFormCustomSelectPref"> Genre </label>
+    <div className='home-container'>
+      <form action='/game' onSubmit={handleSubmit}>
+        <label htmlFor='inlineFormCustomSelectPref'> Genre </label>
         <select value={selectedGenre} onChange={handleGenreSelect}>
-          <option value="" />
+          <option value='' />
           {genres.map((genre) => (
             <option key={genre} value={genre}>
               {genre}
             </option>
           ))}
         </select>
-        <label htmlFor="inlineFormCustomSelectPref"> Number of Artists </label>
+        <label htmlFor='inlineFormCustomSelectPref'> Number of Artists </label>
         <select
           value={artist.artist}
-          defaultValue="2"
+          defaultValue='2'
           onChange={handleArtistSelect}
-          type="number"
+          type='number'
           required
         >
-          <option disabled value="Choose">
+          <option disabled value='Choose'>
             Choose
           </option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
         </select>
-        <label htmlFor="inlineFormCustomSelectPref"> Number of Songs </label>
+        <label htmlFor='inlineFormCustomSelectPref'> Number of Songs </label>
         <select
-          defaultValue="1"
+          defaultValue='1'
           value={song.song}
           onChange={handleSongSelect}
-          type="number"
+          type='number'
           required
         >
-          <option disabled value="Choose">
+          <option disabled value='Choose'>
             Choose
           </option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
         </select>
         <div>
-          <button className="play-btn" type="submit" id="submit" required>
+          <button className='play-btn' type='submit' id='submit' required>
             Play
           </button>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
